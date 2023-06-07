32b6d07b3bc575d70e4f30a63a639cde4db5c585
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-07 11:15:15 -0600

    working alertController

--- MessagesIOS5/MessagesIOS5/Controllers/ViewControllers/MessageTableViewController.swift
+++ MessagesIOS5/MessagesIOS5/Controllers/ViewControllers/MessageTableViewController.swift
@@ -14,6 +14,11 @@ class MessageTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
+    // MARK: - Actions
+    @IBAction func addMessageButtonTapped(_ sender: Any) {
+      presentNewMessageAlert()
+    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MessageController.sharedInstance.messages.count
@@ -36,10 +41,27 @@ class MessageTableViewController: UITableViewController {
         }
     }
     
+    // MARK: - Function
+    func presentNewMessageAlert() {
+        let alert = UIAlertController(title: "New Message", message: "Type your message below hoe", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Your message here hoe..."
+        }
+        let dismissAction = UIAlertAction(title: "Cancel Hoe", style: .cancel)
+        let confirmAction = UIAlertAction(title: "Post Bitch", style: .default) { _ in
+            guard let textField = alert.textFields?.first,
+                  let messageText = textField.text else { return }
+            MessageController.sharedInstance.createMessage(text: messageText)
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(dismissAction)
+        alert.addAction(confirmAction)
+        present(alert, animated: true)
+    }
+    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Thursday, the best day of the year
     }
 } //End of class
-
-// MARK: - Extensions
